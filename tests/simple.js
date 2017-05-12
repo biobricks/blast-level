d0e132f51a6fe10a921a60af1213a9222cb8faad
Yardena Cohen <yardenack@gmail.com>
2017-05-12 13:53:19 -0700

    take db-building out of base
    see, i told you i'd walk this back

--- tests/simple.js
+++ tests/simple.js
@@ -3,23 +3,47 @@ var tape = require('tape');
 
 tape('simple', function(t) {
     t.plan(3)
-    base(function(data) {
-        for (var i in data) {
-            delete data[i].hsps
-        }
-        t.deepEqual(data,[{
-            key: 'foo',
-            value: { seq: 'GATTACACATTACA', updated: 1 },
-            index: undefined
-        },{
-            key: 'bar',
-            value: { seq: 'CATCATCATATTACACATTACCATCATCAT', updated: 1 },
-            index: undefined
-        }],"query data as expected")
+    base(function(db,blastDB) {
+        db.put('foo', {
+            seq: "GATTACACATTACA",
+            updated: 1
+        }, function(err) {
+            t.pass("added foo, building blast index")
+            blastDB.rebuild(function(err) {
+                if(err) t.fail("mysterious failure W: " + err)
+                db.put('bar', {
+                    seq: "CATCATCATATTACACATTACCATCATCAT",
+                    updated: 1
+                }, function(err) {
+                    t.pass("added bar, rebuilding blast index")
+                    blastDB.rebuild(function(err) {
+                        if(err) t.fail("mysterious failure X: " + err)
+                        blastDB.query("ATTACACATTAC", function(err, data) {
+                            if(err) t.fail("mysterious failure Y: " + err)
+                            for (var i in data) {
+                                delete data[i].hsps
+                            }
+                            t.deepEqual(data,[{
+                                key: 'foo',
+                                value: { seq: 'GATTACACATTACA', updated: 1 },
+                                index: undefined
+                            },{
+                                key: 'bar',
+                                value: { seq: 'CATCATCATATTACACATTACCATCATCAT', updated: 1 },
+                                index: undefined
+                            }],"query data as expected")
+                        }, function(err) {
+                            if(err) t.fail("mysterious failure Z: " + err)
+                            t.pass("end of results")
+                        })
+                    })
+                })
+            })
+        })
     },{
         seqProp: 'seq', // key in 'mydb' that stores the sequence data
         changeProp: 'updated',
         rebuild: false, // rebuild the BLAST index when the db is opened
         listen: false, // listen for changes on level db and auto update BLAST db
-    },t)
+    })
 })
